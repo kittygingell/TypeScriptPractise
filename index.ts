bb87f457ae3db9ac3a88c312634395b05512ef59
Kitty Gingell <kittygingell@Kittys-MacBook.local>
2021-06-29 14:28:09 +0100

    input from terminal

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-for (var i=1; i < 101; i++){
+const readline = require('readline-sync');
+
+console.log('\nEnter the maximum number:');
+const Max = readline.prompt();
+
+for (var i=1; i < (parseInt(Max)+1); i++){
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
