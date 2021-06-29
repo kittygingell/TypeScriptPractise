c6018ffb123ab32fb9b623c2bfb2e40427fb5f88
Kitty Gingell <kittygingell@Kittys-MacBook.local>
2021-06-29 14:41:46 +0100

    FizzBuzzBang

--- index.ts
+++ index.ts
@@ -4,8 +4,12 @@ console.log('\nEnter the maximum number:');
 const Max = readline.prompt();
 
 for (var i=1; i < (parseInt(Max)+1); i++){
-    if (i % 15 == 0) console.log("FizzBuzz");
+    if (i % 105 == 0) console.log("FizzBuzzBang");
+    else if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 21 == 0) console.log("FizzBang");
+    else if (i % 35 == 0) console.log("BuzzBang");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
+    else if (i % 7 == 0) console.log("Bang");
     else console.log(i);
 }
